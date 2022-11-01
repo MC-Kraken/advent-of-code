73878633d93f20850882792e1755066e3db248bb
blakemccracken <blake.mccracken@integrate.com>
2022-11-01 13:51:06 -0700

    adding day 4 part 1 c# solution

--- AdventOfCode/Year2015/Day4/Day4.cs
+++ AdventOfCode/Year2015/Day4/Day4.cs
@@ -1,6 +1,41 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace Year2015.Day4;
 
 public class Day4
 {
-    
+    private const string Input = "bgvyzdsv";
+
+    public static void AdventCoinMining()
+    {
+        var number = 0;
+        var flag = true;
+        var md5 = MD5.Create();
+
+        while (flag)
+        {
+            var byteArray = Encoding.UTF8.GetBytes(Input + number);
+            var hash = md5.ComputeHash(byteArray);
+            var convertedHash = Convert.ToHexString(hash);
+
+            Console.WriteLine("checking hash with " + number);
+            Console.WriteLine("first five: " + convertedHash[0] + convertedHash[1] + convertedHash[2] + convertedHash[3] +
+                              convertedHash[4]);
+            
+            if (convertedHash.StartsWith("00000"))
+            {
+                
+                flag = false;
+                Console.WriteLine("you did it!!! Here's the answer: " + number);
+            }
+            else
+            {
+                number++;
+            }
+        }
+
+        
+        Console.WriteLine(number);
+    }
 }
--- AdventOfCode/Year2015/Program.cs
+++ AdventOfCode/Year2015/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Year2015.Day1;
+using Year2015.Day2;
+using Year2015.Day3;
+using Year2015.Day4;
 
-Day1.GetSantaFloor();
+// Day1.GetSantaFloor();
+Day4.AdventCoinMining();
