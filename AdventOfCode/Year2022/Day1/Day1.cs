b7f9e23951948c2824a4b4b7374314b7ddabebce
blakemccracken <blake.mccracken@integrate.com>
2022-12-08 10:56:08 -0800

    2022 day 1 part 1

--- AdventOfCode/Year2022/Day1/Day1.cs
+++ AdventOfCode/Year2022/Day1/Day1.cs
@@ -7,7 +7,23 @@ public class Day1
         var currentDirectory = Directory.GetCurrentDirectory().Split("/bin").First();
         var filePath = Path.Combine(currentDirectory, "Day1/input.txt");
         var text = File.ReadAllLines(filePath);
-        
-        
+
+        var sum = 0;
+        var list = new List<int>();
+        for (var i = 0; i < text.Length; i++)
+        {
+            if (text[i] == "")
+            {
+                list.Add(sum);
+                sum = 0;
+            }
+            else
+            {
+                sum += int.Parse(text[i]);
+            }
+        }
+
+        var highest = list.Max();
+        Console.WriteLine(highest);
     }
 }
